2299f6bb3332a64a605d91e6ef61d3069889f248
Natnaree Asavaseri <bunditnoikiku@hotmail.com>
2018-09-21 15:34:09 +0700

    fix mapping

---
+++ mapping.bash
@@ -0,0 +1,23 @@
+curl -XPUT 'localhost:9200/ku?pretty' -H 'Content-Type: application/json' -d'
+{
+    "settings" : {
+      "number_of_shards": 3,
+      "number_of_replicas": 0
+    },
+    "mappings" : {
+		"webpage": {
+			"properties": {
+				"BaseUrl": {"type" : "keyword"},
+				"Url": {"type" : "keyword"},
+				"Title": {"type" : "text", "analyzer": "thai"},
+				"Body": {"type" : "text", "analyzer": "thai"},
+				"Encoding": {"type" : "keyword"},
+				"Links": {"type" : "keyword"},
+				"Crawling_date": {
+					"type": "date",
+					"format": "yyyy-MM-dd HH:mm:ss"
+				}
+			}
+		}
+ 	 }
+}'
