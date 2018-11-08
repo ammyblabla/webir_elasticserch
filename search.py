83c1d70c06f4c0cd5f07f5508bd298f1aeb285e5
Natnaree Asavaseri <bunditnoikiku@hotmail.com>
2018-11-08 15:32:02 +0700

    add option for summarizer

--- search.py
+++ search.py
@@ -9,7 +9,7 @@ class searcher():
    def __init__(self):
       pass
 
-   def search(self,query):
+   def search(self,query, sum_op = 'url'):
       url = self.localhost + self.search_url + query
       r = requests.get(url)
       res_raw = json.loads(r.text)
@@ -18,7 +18,15 @@ class searcher():
       for one_res in res_raw['hits']['hits']:
          one_res_source = one_res['_source']
          one_res_source['_score'] = one_res['_score']
-         one_res_source['summarize'] = summarizer(one_res_source['link'])
          del one_res_source['remove_stopword_text']
+         
+         if sum_op != 'no':
+            one_res_source['summarize'] = summarizer(one_res_source, op = sum_op)
+         
          search_result.append(one_res_source) 
       return search_result
+
+if __name__ == "__main__":
+   obj = searcher()
+   q = 'note9'
+   print(obj.search(q,sum_op='text'))
--- summarizer.py
+++ summarizer.py
@@ -8,14 +8,23 @@ from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
 
-def summarizer(url, SENTENCES_COUNT=2):
+def summarizer(input_obj, SENTENCES_COUNT=2, op='url'):
    LANGUAGE = "english"
    # SENTENCES_COUNT = 1
    # url =  "https://sea.pcmag.com/smartphones/17424/apple-iphone-x"
-   parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
 
    # text = ' '.join(text.split())
-   # parser = PlaintextParser.from_string(text, Tokenizer(LANGUAGE))
+   # print(input_obj)
+   # print(type(input_obj))
+   parser = None
+   if op == 'text':
+      text = input_obj['text']
+      parser = PlaintextParser.from_string(text, Tokenizer(LANGUAGE))
+   elif op == 'url':
+      url = input_obj['link']
+      parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
+   else:
+      print('OP ERROR')
    stemmer = Stemmer(LANGUAGE)
 
    summarizer = Summarizer(stemmer)
